95d81aabaadb58b22880baafdab3a7e056f44a7d
sugioka03 <sugioka@iret.co.jp>
2025-08-25 13:48:45 +0900

    a

--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 // あなたのCognito設定
-// index.js
-
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_SuFr50b5x",
   client_id: "2jrq9mqinhseiv7qh08t0ds7bg",
-  // Amplifyの公開URLに変更
+  // Amplifyの公開URL
   redirect_uri: "https://main.d2ss8p1mi1643o.amplifyapp.com",
   response_type: "code",
   scope: "openid email",
-  // ログアウト後のリダイレクト先も同様に変更
+  // ログアウト後のリダイレクト先
   post_logout_redirect_uri: "https://main.d2ss8p1mi1643o.amplifyapp.com",
+  // client_idをログアウト処理に含めるための設定
+  extraQueryParams: {
+    client_id: "2jrq9mqinhseiv7qh08t0ds7bg",
+  },
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
